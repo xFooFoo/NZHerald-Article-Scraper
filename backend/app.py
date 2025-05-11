138e84bf6f385be748fd00da6811282c9c01f426
xFooFoo <yarivfu@gmail.com>
2025-05-11 13:38:12 +1200

    Vercel changes

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from NZHScraperFlask import scrapeContent
 import os
